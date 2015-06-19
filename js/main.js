9c3275e784631d4c4d161cdbfb51629dc845fa01
unknown <manuel.cascallar@AR-IT01064.globant.com>
2015-06-19 17:56:18 -0300

    helper functions and shit.

--- js/main.js
+++ js/main.js
@@ -5,32 +5,42 @@ var dunWidth = 40;
 var dunHeight = 40;
 var mainArr = [];
 var cellSize = 20;
-
+var floor = "floor";
+var rock = "rock";
+var wall = "wall"
+var expressAll = massExpress(getTiles());
 
 $(document).ready(function() {
     housekeeping();
+    massExpress(getTiles());
 });
-
-
 
 function housekeeping() {
     for (i = 0; i < dunWidth; i++) {
         mainArr[i] = new Array(dunHeight);
         for (j = 0, len2 = mainArr[i].length; j < len2; j++) {
-            mainArr[i][j] = new cell(i, j, getRandomTileType())
+            mainArr[i][j] = new cell(i, j, "rock")
         }
     }
 }
 
+//returns a cell reference given an array of coordinates
+function cellAt(arrOrX, Y) {
+    if (typeof arrOrX == "object") {
+        return mainArr[arrOrX[0]][arrOrX[1]]
+    } else  if (typeof arrOrX == "number" && typeof Y == "number"){
+        return mainArr[arrOrX][Y]
+    }
+}
 
-//helper function to get a certain type of tile
+//helper function to get a certain type of tile. returns all cells in the floor if no argument is passed.
 function getTiles(type) {
     var allTiles = []
     for (i = 0; i < mainArr.length; i++) {
         for (j = 0, len2 = mainArr[i].length; j < len2; j++) {
             if (mainArr[i][j] instanceof cell) {
                 if (typeof type != "undefined") {
-                    if(mainArr[i][j].cType == type){
+                    if (mainArr[i][j].cType == type) {
                         allTiles.push([mainArr[i][j].cXn, mainArr[i][j].cYn]);
                     }
                 } else {
@@ -42,21 +52,25 @@ function getTiles(type) {
     return allTiles;
 }
 
-
-///////////////TODOOOOOOOO
-function fullExpress(tilesArray){
-    for(let e in tilesArray){
-
+//express an array of cells
+function massExpress(tilesArray) {
+    for (i = 0; i < tilesArray.length; i++) {
+        cellAt(tilesArray[i]).express();
     }
 }
 
+// modify en-masse a collection of cells expressed in an array of coordinates.
+function massModify(tilesArray, property, value) {
+    for (i = 0; i < tilesArray.length; i++) {
+        cellAt(tilesArray[i])[property] = value;
+    }
 
-
+}
 
 ///////////TODOOOOOO
 function getRectCor(x, y, width, height) {
         var rect = [];
-        for (i = 0; i < x; i++) {
+        for (i = 0; i < width; i++) {
             for (j = 0; j < y; j++) {
                 rect.push()
             }
@@ -95,10 +109,13 @@ cell.prototype.express = function() {
             co = '#542437';
             break;
         case "wall":
-            co = '#53777A';
+            co = '#A43F68';
             break;
         case "floor":
             co = '#ECD078';
+            break;
+        case "PING":
+            co = '#C3E90D';
             break;
     }
     ctx.fillStyle = co;
@@ -106,7 +123,18 @@ cell.prototype.express = function() {
 }
 
 var testArr = [
-    [0, 1],
+    [3, 1],
     [0, 2],
-    [0, 3]
+    [5, 3]
 ];
+
+function ping(arrayOrX, y) {
+    var loc = arguments;
+    var save = cellAt(loc).cType;
+    cellAt(loc).cType = "PING";
+    cellAt(loc).express();
+    setTimeout(function() {
+        cellAt(loc).cType = save;
+        cellAt(loc).express();
+    }, 1000)
+}
