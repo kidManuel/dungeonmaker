94d0290c201b7e7713941e01beccd250148377f2
kidManuel <cascallarmanuel@gmail.com>
2017-01-17 00:36:51 -0300

    WIP dungeon.js

--- dev/js/dungeon.js
+++ dev/js/dungeon.js
@@ -1,52 +1,68 @@
 class dungeon {
-	constructor(width, height) {    
-		this.width = width;
-    	this.height = height;
-    	this.cells = [];
-    	this.init();
-	}
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.cells = [];
+        this.init();
+    }
 
-	init(){
-		for (var i = 0; i < this.width; i++) {
-        	this.cells[i] = [];
-        	for (var j = 0; j < this.height; j++) {
-            	this.cells[i][j] = new cell(i, j, 'rock');
-        	}
+    init() {
+        for (var i = 0; i < this.width; i++) {
+            this.cells[i] = [];
+            for (var j = 0; j < this.height; j++) {
+                this.cells[i][j] = new cell(i, j, 'rock');
+            }
         }
-	}
+    }
 
-	findTiles(filter, array) {
-    	var selection = [];
-    	var cells = array || this.cells;
+    findTiles(filter, array) {
+        var selection = [];
+        var cells = array || this.cells;
 
-    	
-		//Review after array.prototype.flatten
-    	if(!filter && !array) {
-    		selection = cells.reduce(function(a, b) {
-  				return a.concat(b);
-			})
-			return selection;
-    	}
 
-    	cells.iterate(
-    		function() {
-    			if(dungeon.cellIsType(this, filter)) {
-    				selection.push(this)
-    			}
-    		}
-    	)
+        //Review after array.prototype.flatten
+        if (!filter && !array) {
+            selection = cells.reduce(function(a, b) {
+                return a.concat(b);
+            });
+            return selection;
+        }
 
-	    return selection;
-	}
+        cells.iterate(function() {
+                if (dungeon.cellIsType(this, filter)) {
+                    selection.push(this);
+                } //a
+            } //b
+        ); // c
+        return selection;
+    } //d
 
-	//Move to cell ?
-	static cellIsType(testCell, filter) {
-		return (testCell instanceof cell && testCell.type === filter);
-	}
+    massExpress(tilesArray) {
+        for (var i = 0; i < tilesArray.length; i++) {
+            this.cellAt(tilesArray[i].X, tilesArray[i].Y).express();
+        }
+    }
 
-	massExpress(tilesArray) {
-	    for (var i = 0; i < tilesArray.length; i++) {
-	        this.cellAt(tilesArray[i].X, tilesArray[i].Y).express();
-	}
-	
+    static findNeighbours(cell) {
+        var neighbours = [];
+        var x = cell.X;
+        var y = cell.Y;
+        if (this.cellAt(x, y - 1)) {
+            neighbours.push(this.cells[x, y - 1]);
+        }
+        if (this.cellAt(x + 1, y)) {
+            neighbours.push(this.cells[x + 1, y]);
+        }
+        if (this.cellAt(x, y + 1)) {
+            neighbours.push(this.cells[x, y + 1]);
+        }
+        if (this.cellAt(x - 1, y)) {
+            neighbours.push(this.cells[x - 1, y]);
+        }
+        return neighbours;
+    }
+
+    static cellIsType(testCell, filter) {
+        return (testCell instanceof cell && testCell.type === filter);
+    }
 }
