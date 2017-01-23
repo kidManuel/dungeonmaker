20777f45cab5dcae550e0e439dbeff61893b17a5
Marie Veyran <Marieveyran@MacBook-Pro-de-Marie-Veyran.local>
2017-01-22 23:40:53 -0300

    Housework.

--- dev/js/gameMain.js
+++ dev/js/gameMain.js
@@ -2,20 +2,21 @@ document.addEventListener('DOMContentLoaded', function(event) {
     document.removeEventListener('DOMContentLoaded', arguments.callee, false);
     Object.freeze(window.params = userParams ? userParams : defaultParams);
     window.game = new GameMain(params);
+    game.afterReady();
 });
 
 class GameMain {
     constructor() {
-        this.density = params.density; // ammount of attempts at filling the floor.
         this.graphics = new GraphicsController();
         window.graphics = this.graphics;
         this.dungeon = new DungeonFloor(params.dunWidth, params.dunHeight);
+        this.mouse = new MouseController();
         if (params.devMode) {
             window.dun = this.dungeon;
         }
     }
 
-    
-
-
+    afterReady() {
+        //review sometime
+    }
 }
