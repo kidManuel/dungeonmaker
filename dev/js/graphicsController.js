870668bc65f50c27a79e1b492d991c5cedd5eadb
Marie Veyran <Marieveyran@MacBook-Pro-de-Marie-Veyran.local>
2017-01-22 23:38:15 -0300

    Add render capabilities

--- dev/js/graphicsController.js
+++ dev/js/graphicsController.js
@@ -1,14 +1,31 @@
+// figure out what do we do with this 
+
+spriteSheet = new Image();
+spriteSheet.src = 'imgs/spr.png';
+
 class GraphicsController {
-	constructor() {
-		this.widthPixels = params.dunWidth * params.cellSize; //size in pixels of the floor.
+    constructor() {
+        this.widthPixels = params.dunWidth * params.cellSize; //size in pixels of the floor.
         this.heightPixels = params.dunHeight * params.cellSize;
         this.allCanvas = document.querySelectorAll('canvas');
         this.layout = document.getElementById('layout').getContext('2d');
+        this.cursorElems = document.getElementById('cursor');
         this.mainContainer = document.getElementById('mainContainer');
+        this.options = {
+            //review when xhr solution rolls around -__-
+            'crosshair': {
+                source: spriteSheet,
+                sourceX: 0,
+                sourceY: 0,
+                width: 15,
+                height: 15,
+                context: this.cursorElems.getContext('2d')
+            }
+        }
         this.initializeCanvases();
-	}
+    }
 
-	initializeCanvases() {
+    initializeCanvases() {
         this.mainContainer.style.width = this.widthPixels + 'px';
         this.mainContainer.style.height = this.heightPixels + 'px';
 
@@ -19,15 +36,30 @@ class GraphicsController {
     }
 
     drawTile(x, y, floorType) {
-    	//review once sprites roll around
-    	var pallette = {
-    		'rock':'#542437',
-			'wall':'#A43F68',
-			'floor':'#ECD078',
-			'PING':'#C3E90D'
-    	}
-    	var color = pallette[floorType];
-    	this.layout.fillStyle = color;
-    	this.layout.fillRect(x, y, params.cellSize, params.cellSize);
+        //review once sprites roll around
+        var pallette = {
+            'rock':'#542437',
+            'wall':'#A43F68',
+            'floor':'#ECD078',
+            'PING':'#C3E90D'
+        }
+        var color = pallette[floorType];
+        this.layout.fillStyle = color;
+        this.layout.fillRect(x, y, params.cellSize, params.cellSize);
+    }
+
+    render(imageToDraw, x, y) {
+        let target = this.options[imageToDraw];
+        target.context.drawImage(
+            target.source,
+            target.sourceX,
+            target.sourceY,
+            target.width,
+            target.height,
+            x * params.cellSize,
+            y * params.cellSize,
+            params.cellSize,
+            params.cellSize
+        );
     }
 }
