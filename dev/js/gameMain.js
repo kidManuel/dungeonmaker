e191e693dd4c45b59ed107026847536aea6a7655
kidManuel <cascallarmanuel@gmail.com>
2017-01-29 22:13:24 -0300

    New object structure

--- dev/js/gameMain.js
+++ dev/js/gameMain.js
@@ -2,18 +2,20 @@ document.addEventListener('DOMContentLoaded', function(event) {
     document.removeEventListener('DOMContentLoaded', arguments.callee, false);
     Object.freeze(window.globalparams = userParams ? userParams : defaultParams);
     window.game = new GameMain(globalparams);
-    game.afterReady();
 });
 
 class GameMain {
     constructor() {
         this.graphics = new GraphicsController();
-        window.graphics = this.graphics;
-        this.dungeonGen = new DungeonGenerator(globalparams.dunWidth, globalparams.dunHeight);
+        this.dungeonGen = new DungeonGenerator();
         this.mouse = new MouseController();
+        this.floor = this.dungeonGen.generateFloor(globalparams.dunWidth, globalparams.dunHeight);
+        this.graphics.massExpress(this.floor);
         if (globalparams.devMode) {
-            window.dun = this.dungeon;
+            window.dun = this.floor;
         }
+
+        //this.afterReady();
     }
 
     afterReady() {
