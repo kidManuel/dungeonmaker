9c79c92071afeecbf0119b35194979529b11dc76
kidManuel <cascallarmanuel@gmail.com>
2017-02-07 01:53:25 -0300

    Needed string proto function.

--- dev/js/arrayUtils.js
+++ dev/js/arrayUtils.js
@@ -1,3 +1,7 @@
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 Array.prototype.randomElement = function() {
     return this[Math.floor(Math.random() * this.length)]
 }
