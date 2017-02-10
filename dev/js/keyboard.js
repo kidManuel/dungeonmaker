759bf0267e54c2fd551b517c6b2fbf0336b5a6a9
kidManuel <cascallarmanuel@gmail.com>
2017-02-10 02:50:07 -0300

    keyboard startup

---
+++ dev/js/keyboard.js
@@ -0,0 +1,23 @@
+class KeyboardController extends Speaker {
+    constructor(comunications) {
+        super(comunications);
+        var me = this;
+        window.addEventListener('keydown', function() { me.keydown(event) }, false);
+    }
+    keydown(event) {
+        switch (event.code) {
+            case 'KeyW': // Left
+                this.dispatch('keyMoveUp');
+                break;
+            case 'KeyA': // Up
+                this.dispatch('keyMoveLeft');
+                break;
+            case 'KeyS': // Right
+                this.dispatch('keyMoveDown');
+                break;
+            case 'KeyD': // Down
+                this.dispatch('keyMoveRight');
+                break;
+        }
+    }
+}
