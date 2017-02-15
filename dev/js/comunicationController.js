6ee9a0018fd5c7256fd6d0f059eac5c61dbe4e75
kidManuel <cascallarmanuel@gmail.com>
2017-02-15 00:53:06 -0300

    Clearer topic creation.

--- dev/js/comunicationController.js
+++ dev/js/comunicationController.js
@@ -6,7 +6,7 @@ class ComunicationController {
     registerListener(object, topic) {
         if (!this.topics[topic]) {
             this.topics[topic] = [object]
-            devmode && console.log('creating new topic ')
+            devmode && console.log('creating new topic: ' + topic)
         } else {
             this.topics[topic].push(object);
         }
