c85e4fe9420c071d5ed6e1e2b437e5353e1792cd
kidManuel <cascallarmanuel@gmail.com>
2017-02-21 22:52:39 -0300

    Final (for now) dungeon generation algorithm + change settings for impact.

--- dev/config/defaultParams.js
+++ dev/config/defaultParams.js
@@ -1,11 +1,11 @@
 var userParams = null;
 var defaultParams = {
-    'dunWidth': 40, //size in cells of the floor.
-    'dunHeight': 40,
-    'minRoomWidth': 6,
-    'minRoomHeight': 6,
-    'maxRoomWidth': 6,
-    'maxRoomHeight': 6,
+    'dunWidth': 100, //size in cells of the floor.
+    'dunHeight': 100,
+    'minRoomWidth': 3,
+    'minRoomHeight': 3,
+    'maxRoomWidth': 10,
+    'maxRoomHeight': 10,
     'cellSize': 34,
     'density': 60, // ammount of attempts at filling the floor.
     'devMode': true
--- dev/js/dungeon.js
+++ dev/js/dungeon.js
@@ -19,66 +19,101 @@ class DungeonGenerator {
         let minWid = globalparams.minRoomWidth;
         let minHei = globalparams.minRoomHeight;
         let attempts = globalparams.density;
-        let unconnected = [];
-        let pathsTiles = []; 
+        let rooms = [];
+        let connected = [];
+        let distances = [];
+        let pathsTiles = [];
+        let me = this;
 
         for (var i = 0; i < attempts; i++) {
             var randWid = Math.floor(Math.random() * (maxWid - minWid + 1) + minWid);
             var randHei = Math.floor(Math.random() * (maxHei - minHei + 1) + minHei);
             var randX = Math.floor(Math.random() * (width - randWid - 1)) + 1;
             var randY = Math.floor(Math.random() * (height - randHei - 1)) + 1;
-
             let target = floor.getPlane(randWid, randHei, randX, randY);
-
             if (target.isAvailable()) {
                 this.readyCells.merge(target);
                 this.massModify(target, 'floor', 'floor');
-                let wallsX = randX ? randX - 1 :  0;
-                let wallsY = randY ? randY - 1 :  0;
+                let wallsX = randX ? randX - 1 : 0;
+                let wallsY = randY ? randY - 1 : 0;
                 this.massModify(floor.getRect(randWid + 1, randHei + 1, wallsX, wallsY), 'floor', 'wall');
-                unconnected.push(target.centerPoint());
+                rooms.push(target.centerPoint());
             }
         }
 
-        while (unconnected.length > 0) {
-            let maxDistance = 1000;
-            let closest = 0;
-            let seed = Math.floor(Math.random() * unconnected.length);
-            let objective = unconnected[seed];
-
-            //Review once 'getClosest' rolls around.
-            for (var j = 0; j < unconnected.length; j++) {
-                if (seed == j){
-                    continue;
+        //calculate each individual distance between two nodes only once
+        rooms.forEach(function(initial, iIndex) {
+            rooms.forEach(function(objective, oIndex) {
+                if (initial !== objective && !objective.calculatedDistances.includes(initial)) {
+                    distances.push([me.getManhattanDistance(initial, objective), initial, objective]);
+                    initial.calculatedDistances.push(objective);
+                    objective.calculatedDistances.push(initial)
                 }
-                var manhDistance = this.getManhattanDistance(unconnected[j], objective);
-                if (manhDistance < maxDistance) {
-                    maxDistance = manhDistance;
-                    closest = j;
+            })
+        })
+
+        //sort all the possible distances 
+        distances.sort(function(a, b) {
+            return a[0] - b[0]
+        });
+
+        //arbitrarily add one of the rooms to the "main loop" of rooms
+        rooms[0].inMainLoop = true;
+
+        //as long as we have rooms unconected to the "main loop"
+        //execute the shortest posible conection between two rooms
+        while (rooms.some(function(room) {return !room.inMainLoop})) {
+            let initial = distances[0][1];
+            let objective = distances[0][2];
+            if (!objective.connections.includes(initial)) {
+                //connect initial and obejctive
+                //all connections with initial are now merged with the connections to objective.
+                mergeConnections(initial, objective);
+                connectRooms(initial, objective);
+
+                //if ANY of all rooms now connected, is in the "main loop", add everything to the main loop
+                if(initial.connections.some(function(connection){
+                    return connection.inMainLoop;
+                })){
+                    initial.inMainLoop = true;
+                    initial.connections.iterate(function(connection){
+                        connection.inMainLoop = true;
+                    })
                 }
             }
+            //either the rooms are already connected and we don't need the connection
+            //or the connection is executed and we don't need the data anymore
+            distances.removeIndex(0);
+        }
 
-            let tempPath = floor.simplePath(objective, unconnected[closest]);
+        function mergeConnections(a, b) {
+            let connectedRooms = a.connections.merge(b.connections);
+            connectedRooms.push(a, b);
+            connectedRooms.iterate(function(singleRoom) {
+                singleRoom.connections.merge(connectedRooms);
+            })
+        }
+
+        function connectRooms(a, b) {
+            let tempPath = floor.simplePath(a, b);
             pathsTiles.attatch(tempPath);
-            this.readyCells.merge(tempPath);
-            this.massModify(tempPath, 'floor', 'floor');
-            unconnected.splice(seed, 1);
+            me.readyCells.merge(tempPath);
+            me.massModify(tempPath, 'floor', 'PING');
         }
+
         this.polish(floor, pathsTiles);
         return floor;
     }
-
     polish(zone, subsection) {
         // Polishes an entire zone, or a subsection of that zone
         // Returns the now updated cells
         var tilesToPolish = subsection || zone;
         var needsUpdate = [];
-
         tilesToPolish.iterate(
-            function(tile){
+            function(tile) {
                 let neighbors = zone.findNeighbors(tile);
                 neighbors.iterate(
-                    function(singleNeighbor){
+                    function(singleNeighbor) {
                         if (singleNeighbor.floor === 'rock') {
                             singleNeighbor.floor = 'wall';
                             needsUpdate.push(singleNeighbor);
@@ -89,7 +124,6 @@ class DungeonGenerator {
         )
         return needsUpdate;
     }
-
     massModify(tilesArray, property, value) {
         tilesArray.iterate(
             function(tile) {
@@ -97,18 +131,17 @@ class DungeonGenerator {
             }
         )
     }
-
     checkAvailable(room) {
-        if (Array.isArray(room)){         
+        if (Array.isArray(room)) {
             return room.every(
-                function(tile){
+                function(tile) {
                     return (tile instanceof Terrain && tile.floor !== 'floor' && tile.floor !== 'wall')
                 }
             )
         } else if (room instanceof Terrain) {
             return (room.floor !== 'floor' && room.floor !== 'wall')
         } else {
-            throw new Error ('can\'t check that, brosky: ' + Array.from(arguments))
+            throw new Error('can\'t check that, brosky: ' + Array.from(arguments))
         }
     }
     findNearestIn(cell, candidates) {
@@ -120,7 +153,6 @@ class DungeonGenerator {
         })
         return closest;
     }
-
     getManhattanDistance(a, b) {
         //gridutils
         return (Math.abs(a.x - b.x) + Math.abs(a.y - b.y));
